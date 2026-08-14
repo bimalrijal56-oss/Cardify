6ef23222fdf0737bcab3bf2afc7c8c1313002568
bimalrijal56-oss <bimalrijal56@gmail.com>
2026-08-14 23:17:17 +0545

    fixed middleware

--- cardapp/middleware.py
+++ cardapp/middleware.py
@@ -1,11 +1,21 @@
+from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 
 
 class MediaCORSMiddleware(MiddlewareMixin):
 
     def process_response(self, request, response):
-
         if request.path.startswith("/media/"):
-            response["Access-Control-Allow-Origin"] = "*"
+            origin = request.headers.get("Origin")
+            allowed_origins = getattr(settings, "CORS_ALLOWED_ORIGINS", [])
+
+            if origin and origin in allowed_origins:
+                response["Access-Control-Allow-Origin"] = origin
+                response["Vary"] = "Origin"
+            elif origin:
+                response["Access-Control-Allow-Origin"] = origin
+                response["Vary"] = "Origin"
+            else:
+                response["Access-Control-Allow-Origin"] = "*"
 
         return response
